394ab26d14fb073da54cbfc60adcc8be08786d8f
apoorvkhare07 <apoorvkhare007@gmail.com>
2023-05-04 15:08:17 +0530

    Updates

---
+++ precache-manifest.a234fcb77a5e41a67f296a14bdfbc0e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "eff060e7d0c91c4fc34b",
+    "url": "/website-v2/static/css/main.f7de8607.chunk.css"
+  },
+  {
+    "revision": "eff060e7d0c91c4fc34b",
+    "url": "/website-v2/static/js/main.eff060e7.chunk.js"
+  },
+  {
+    "revision": "b45934e459dfbeaa643c",
+    "url": "/website-v2/static/css/1.6a6cf379.chunk.css"
+  },
+  {
+    "revision": "b45934e459dfbeaa643c",
+    "url": "/website-v2/static/js/1.b45934e4.chunk.js"
+  },
+  {
+    "revision": "24e4aaf33af8ecabe13b",
+    "url": "/website-v2/static/js/runtime~main.24e4aaf3.js"
+  },
+  {
+    "revision": "e49dfa5c27dd5e2141584b8d3e5fc970",
+    "url": "/website-v2/static/media/dataphi.e49dfa5c.png"
+  },
+  {
+    "revision": "6b53d5324242eb54ae04af627258da3a",
+    "url": "/website-v2/static/media/iitrlogo.6b53d532.png"
+  },
+  {
+    "revision": "285c76a564b76afe9bf88d231ebaeed4",
+    "url": "/website-v2/static/media/andromeda.285c76a5.png"
+  },
+  {
+    "revision": "496a18e0b06f101fb7e688a0eb430d04",
+    "url": "/website-v2/static/media/theme.496a18e0.png"
+  },
+  {
+    "revision": "377604d2d91a778c561782ae8f4acb88",
+    "url": "/website-v2/static/media/me.377604d2.jpg"
+  },
+  {
+    "revision": "8285d7efa9c873e457b251b6db985703",
+    "url": "/website-v2/static/media/dev_cv.8285d7ef.pdf"
+  },
+  {
+    "revision": "1a54d048cfbddbeebe4db5db6c8f5c44",
+    "url": "/website-v2/static/media/icons8-github.1a54d048.svg"
+  },
+  {
+    "revision": "04f5685a58ffe9fc7f8b028e060a7a18",
+    "url": "/website-v2/static/media/icons8-twitter.04f5685a.svg"
+  },
+  {
+    "revision": "07961c853cbcc5598c1d5008bb2d8441",
+    "url": "/website-v2/static/media/icons8-linkedin.07961c85.svg"
+  },
+  {
+    "revision": "3755681bc301e057aff71a68ba435706",
+    "url": "/website-v2/static/media/icons8-mail.3755681b.svg"
+  },
+  {
+    "revision": "ef642919c85d1fffa8df8a8d164bb3a5",
+    "url": "/website-v2/index.html"
+  }
+];
